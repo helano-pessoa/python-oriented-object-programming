e1e9572be236a0a789b9367793da168cd0d31518
helano-pessoa <hp@alu.ufc.br>
2021-02-15 21:48:26 -0300

    Accomplish chapter 1

---
+++ Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/definition_start.py
@@ -0,0 +1,17 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Basic class definitions
+
+
+# TODO: create a basic class
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+# TODO: create instances of the class
+b1 = Book("A Travel Through Time")
+b2 = Book("Christmas Carol")
+
+# TODO: print the class and property
+print(b1)
+print(b1.title)
+
